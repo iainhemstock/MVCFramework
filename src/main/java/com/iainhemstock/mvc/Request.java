11f36b28da12c26ba95564eed322510ef088bc98
unknown <iainhemstock@googlemail.com>
2018-12-02 19:37:51 +0000

    convert request and response objs to interfaces

--- src/main/java/com/iainhemstock/mvc/Request.java
+++ src/main/java/com/iainhemstock/mvc/Request.java
@@ -1,13 +1,6 @@
 package com.iainhemstock.mvc;
 
-public class Request {
-    private String body;
-
-    public void setBody(String json) {
-        this.body = json;
-    }
-
-    public String getBody() {
-        return body;
-    }
+public interface Request {
+    void setParameter(String key, Object value);
+    Object getParameter(String key);
 }
